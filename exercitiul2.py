6c6a0d40ad736274e6b8a2b48e28a845bc4b9cb0
CristinaPadurariu <cristinapadurariu7@gmail.com>
2020-12-07 21:17:42 +0200

    Mid

---
+++ exercitiul2.py
@@ -0,0 +1,12 @@
+lista_nume = ['Maria', 'Irina', 'Claudiu', 'Ionut', 'Irina', 'Matei', 'Irina', 'Maria', 'Claudiu'] #Sortare dupa nume
+lista_nume.sort()
+print "Sortare dupa nume: ", lista_nume
+
+
+#de cate ori apare un nume in lista
+
+lista_nume.count(Maria)
+lista_nume.count(Irina)
+lista_nume.count(Claudiu)
+lista_nume.count(Ionut)
+lista_nume.count(Matei)
---
+++ mid.py
@@ -0,0 +1,13 @@
+string = "The Inquisitor must meet Varric on top of Skyhold's battlements to be introduced."
+print(string)
+
+patches = [[5, 14, "Conquistador"], [26, 31, "King"], [43, 49, "Palace"]]
+
+print(string[0:4]; [15:25]; [27:30]; [32:42]; [50])
+
+x[5:14]= "Conquistador"
+y[26:31]= "King"
+z[43:49]= "Palace"
+
+
+
