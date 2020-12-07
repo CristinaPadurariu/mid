b92dbc8cd07054aad28ea26af8a53fd8f4ac6c90
CristinaPadurariu <cristinapadurariu7@gmail.com>
2020-12-07 21:31:25 +0200

    mid

--- exercitiul2.py
+++ exercitiul2.py
@@ -1,12 +1,30 @@
 lista_nume = ['Maria', 'Irina', 'Claudiu', 'Ionut', 'Irina', 'Matei', 'Irina', 'Maria', 'Claudiu'] #Sortare dupa nume
 lista_nume.sort()
-print "Sortare dupa nume: ", lista_nume
+print("Sortare dupa nume: ", lista_nume)
 
 
 #de cate ori apare un nume in lista
+for Maria in lista_nume:
+    count = 0
+    count = +1
+    print(lista_nume.count(Maria))
 
-lista_nume.count(Maria)
-lista_nume.count(Irina)
-lista_nume.count(Claudiu)
-lista_nume.count(Ionut)
-lista_nume.count(Matei)
+for Irina in lista_nume:
+    count = 0
+    count = +1
+    print(lista_nume.count(Irina))
+
+for Claudiu in lista_nume:
+    count = 0
+    count = +1
+    print(lista_nume.count(Claudiu))
+
+for Ionut in lista_nume:
+    count = 0
+    count = +1
+    print(lista_nume.count(Ionut))
+
+for Matei in lista_nume:
+    count = 0
+    count = +1
+    print(lista_nume.count(Matei))
